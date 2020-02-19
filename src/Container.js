34cd3aee80c23fe7cf9c8a8fd2c3fd42a0089a66
Elias H <falastiny91@hhotmail.com>
2020-02-19 17:37:13 +0100

    Fixed Grid System & Added Cards

--- src/Container.js
+++ src/Container.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import Header from './Header'
 import Footer from './Footer'
+import Card from './Card'
 
 
-function Container(){
-  return(
+function Container() {
+  return (
     <div>
-    <Header />
-    <Footer />
+      <Header />
+
+      <div class="container-fluid">
+
+        <div class="row">
+          <div class="col">
+          <Card />
+          </div>
+          <div class="col">
+          <Card />
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+          <Card />
+          </div>
+          <div class="col">
+          <Card />
+          </div>
+
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
+
+
 
 export default Container;
